97ab83875393995637d71f3be795f81deb435643
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-11-15 23:19:30 +0600

    db connection successfull and get all books from db is done

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express()
-
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 
+// Bookory_DB
+// gVGKVqwKbuoAdNiT
+
 app.use(cors())
 app.use(express.json())
+
+const uri = "mongodb+srv://Bookory_DB:gVGKVqwKbuoAdNiT@cluster0.agg0z.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect()
+        const booksCollection = client.db('book_info').collection('books')
+
+        app.get('/books', async(req, res)=>{
+            const result = await booksCollection.find({}).toArray()
+            res.send(result)
+        })
+
+    }
+    finally{
+        // await client.close()
+    }
+}
+
+run().catch(console.dir)
+
 
 app.get('/', (req, res)=>{
     res.send('The Bookory Server is running')
